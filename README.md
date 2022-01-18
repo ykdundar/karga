77daf5e6c05665375f2a456e26c53fee66227bdb
dundaryesim <kuranyesim@gmail.com>
2022-01-18 22:12:13 +0100

    Add info to README.md

--- README.md
+++ README.md
@@ -2,7 +2,11 @@
 
 ## Install
 
-First, obtain an API Key for [AlphaVantage](https://www.alphavantage.co/support/#api-key) and then define an environment variable named as `KARGA_TOKEN` as follows:
+```
+go get -u github.com/ykdundar/karga
+```
+
+Then, obtain an API Key for [AlphaVantage](https://www.alphavantage.co/support/#api-key) and then define an environment variable named as `KARGA_TOKEN` as follows:
 
 ```
 export KARGA_TOKEN=YOUR_TOKEN
--- cmd/quote.go
+++ cmd/quote.go
@@ -17,8 +17,8 @@ var quoteCmd = &cobra.Command{
 	Long: `This command returns the price and volume information for a given stock.
 
 For example:
-
 Open, High, Low, Price, Volume, etc.`,
+
 	Run: func(cmd *cobra.Command, args []string) {
 		fmt.Println(internal.GlobalQuoteRequest(symbol))
 	},
@@ -29,13 +29,4 @@ func init() {
 
 	quoteCmd.PersistentFlags().StringVarP(&symbol, "symbol", "s", "IBM", "Stock symbol. I.e. IBM")
 
-	// Here you will define your flags and configuration settings.
-
-	// Cobra supports Persistent Flags which will work for this command
-	// and all subcommands, e.g.:
-	// quoteCmd.PersistentFlags().String("foo", "", "A help for foo")
-
-	// Cobra supports local flags which will only run when this command
-	// is called directly, e.g.:
-	// quoteCmd.Flags().BoolP("toggle", "t", false, "Help message for toggle")
 }
